8e8704c7d22d075de0a694e6f0e7abd14c5ca6ae
danielletate <88792165+danielletate@users.noreply.github.com>
2021-08-23 15:41:47 -0600

    Created cat generator button

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-
+function generateCat() {
+  let image = document.createElement("img");
+  let div = document.getElementById("flex-cat-gen");
+  let catImagesURL =
+    "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  catImagesURL.src = catImagesURL;
+  div.appendChild(image);
+}
